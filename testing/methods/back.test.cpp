35edc548935a94d33830c30904465c0ba9f3ad91
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-06 12:02:16 -0500

    feat: update tests for back function

--- testing/methods/back.test.cpp
+++ testing/methods/back.test.cpp
@@ -1,31 +1,31 @@
 #include <gtest/gtest.h>
-#include "../helpers/intlist.typealias.helper.h"
+#include "intlist.typealias.helper.h"
 
-TEST(back, shoudlReturnTheBack)
+TEST(back, should_return_the_back)
 {
-	const int valueExpected = 3;
+	const int value_expected = 3;
 	intlist list;
 	list.push_back(1);
 	list.push_back(2);
 	list.push_back(3);
-	EXPECT_EQ(list.back(), valueExpected);
+	EXPECT_EQ(list.back(), value_expected);
 }
 
-TEST(back, shouldThrowLengthError)
+TEST(back, should_throw_length_error)
 {
-	intlist emptylist;
-	EXPECT_THROW(emptylist.back(), std::length_error);
+	intlist empty_list;
+	EXPECT_THROW(empty_list.back(), std::length_error);
 }
 
-TEST(back_constlist, shouldReturnTheBack)
+TEST(back_constlist, should_return_the_back)
 {
 	const int back = 10;
-	const intlist list{ 1,2,back };
+	const intlist list{ 1, 2, back };
 	EXPECT_EQ(list.back(), back);
 }
 
-TEST(back_constlist, shouldThrowLengthError)
+TEST(back_constlist, should_throw_length_error)
 {
-	const intlist emptylist;
-	EXPECT_THROW(emptylist.back(), std::length_error);
+	const intlist empty_list;
+	EXPECT_THROW(empty_list.back(), std::length_error);
 }
