dcbda729393cdf9a8c49f96bf51be93376258ba1
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-06 11:34:03 -0500

    feat: add and update tests for clear function

--- testing/methods/clear.test.cpp
+++ testing/methods/clear.test.cpp
@@ -1,37 +1,41 @@
 #include <gtest/gtest.h>
-#include "../helpers/intlist.typealias.helper.h"
+#include "intlist.typealias.helper.h"
+#include "compare-list.helper.h"
 
-TEST(destructor_clear, noProblemUsingDestructorAfterClear)
+TEST(clear, should_clear_the_list_succesfully)
 {
-	{
-		intlist list{ 1, 2, 3 };
-		list.clear();
-	}
-	EXPECT_EQ(1, 1);
+	intlist list{ 1,2,3,4,5 };
+	ASSERT_NO_THROW(list.clear());
+	EXPECT_TRUE(list.empty());
 }
 
-TEST(destructor_clear, nelmsShouldBeZero)
+TEST(clear, should_not_throw_an_error_if_clear_is_called_two_times)
+{
+	intlist list{ 1,2,3 };
+	ASSERT_NO_THROW(list.clear());
+	ASSERT_NO_THROW(list.clear());
+	EXPECT_TRUE(true);
+}
+
+TEST(clear, nelms_should_be_zero_after_call)
 {
 	intlist list{ 1, 2, 3 };
 	list.clear();
-	std::cout << list.size();
+	EXPECT_EQ(list.size(), 0);
 }
 
-TEST(destructor_clear, noProblemIfListItsEmpty)
+TEST(clear, should_not_throw_error_if_list_empty)
 {
 	intlist list;
-	list.clear();
-	EXPECT_EQ(1, 1);
+	ASSERT_NO_THROW(list.clear());
 }
 
-TEST(destructor_clear, canPushAfterClearWithNoProblem)
+TEST(clear, should_be_able_to_add_elements_after_call)
 {
 	intlist list{ 1, 2, 3 };
 	list.clear();
-	list.push_back(1);
-	list.push_back(2);
-	list.push_back(3);
-	ASSERT_EQ(list.size(), 3);
-	ASSERT_EQ(list.front(), 1);
-	ASSERT_EQ(list.back(), 3);
+	ASSERT_NO_THROW(list.push_back(0));
+	ASSERT_NO_THROW(list.push_back(1));
+	ASSERT_NO_THROW(list.push_back(0));
+	EXPECT_TRUE(compare_list(list, { 0, 1, 0 }));
 }
