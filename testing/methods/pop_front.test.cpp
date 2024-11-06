1a5064501b6c0c1f6806f204b76e5cb0d13a5a70
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-06 16:33:33 -0500

    feat: improve pop_front function tests readability

--- testing/methods/pop_front.test.cpp
+++ testing/methods/pop_front.test.cpp
@@ -1,27 +1,24 @@
 #include <gtest/gtest.h>
 #include "../helpers/intlist.typealias.helper.h"
 
-TEST(pop_front, shouldPopTheFront)
+TEST(pop_front, should_pop_front)
 {
-	intlist list;
-	list.push_back(1);
-	list.push_back(2);
-	list.push_back(3);
-
+	const int aux = 2;
+	intlist list{ 1,aux };
 	list.pop_front();
-	EXPECT_EQ(list.front(), 2);
+	EXPECT_EQ(list.front(), aux);
 }
 
-TEST(pop_front, ifListEmptyShouldThrowException)
+TEST(pop_front, should_throw_length_error_if_list_empty)
 {
-	intlist emptylist;
-	EXPECT_THROW(emptylist.pop_front(), std::length_error);
+	intlist empty_list;
+	EXPECT_THROW(empty_list.pop_front(), std::length_error);
 }
 
-TEST(pop_front, shouldChangeSize)
+TEST(pop_front, should_update_nelms)
 {
 	intlist list{ 1,2,3 };
-	const auto currentSize = list.size();
+	const auto current_size = list.size();
 	list.pop_front();
-	EXPECT_EQ(list.size(), currentSize - 1);
+	EXPECT_EQ(list.size(), current_size - 1);
 }
