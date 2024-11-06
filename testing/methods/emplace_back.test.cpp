27a25a86dacfb6ccfc1c626978d29c3a315e5241
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-06 13:16:44 -0500

    feat: update and fix emplace_back function test

--- testing/methods/emplace_back.test.cpp
+++ testing/methods/emplace_back.test.cpp
@@ -2,7 +2,7 @@
 #include "../helpers/intlist.typealias.helper.h"
 #include "../helpers/resource.helper.h"
 
-TEST(emplace_back, shouldInsertInTheBack)
+TEST(emplace_back, should_insert_succesfully)
 {
 	const int newvalue = 4;
 	intlist testlist{ 1,2,3 };
@@ -10,14 +10,17 @@ TEST(emplace_back, shouldInsertInTheBack)
 	EXPECT_EQ(testlist.back(), newvalue);
 }
 
-TEST_F(test_resource_list, dontCopyUsingEmplaceBack)
+TEST_F(test_resource_list, emplace_back_should_not_copy)
 {
+	// constructs a test_resource(100)
 	test_list.emplace_back(100);
 	EXPECT_EQ(test_resource::instances_created, 1);
 }
 
-TEST_F(test_resource_list, updateNelmsUsingEmplaceBack)
+TEST(emplace_back, should_update_nelms)
 {
-	test_list.emplace_back(100);
-	EXPECT_EQ(test_list.size(), 1);
+	intlist list{ 1,2,3 };
+	const std::size_t current_size = list.size();
+	list.emplace_back(4);
+	EXPECT_EQ(list.size(), current_size + 1);
 }
