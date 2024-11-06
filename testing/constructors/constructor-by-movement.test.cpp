8ec71c623b7e3a5ab44f7426743e65602eb40570
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-06 17:17:57 -0500

    feat: improve constructor by mov tests readability

--- testing/constructors/constructor-by-movement.test.cpp
+++ testing/constructors/constructor-by-movement.test.cpp
@@ -1,22 +1,22 @@
 #include <gtest/gtest.h>
-#include "../helpers/intlist.typealias.helper.h"
-#include "../helpers/resource.helper.h"
+#include "intlist.typealias.helper.h"
+#include "resource.helper.h"
 
-TEST(constructorByMovement, shouldUpdateNelms)
-{
-	intlist list{ 1,2,3 };
-	intlist list2 = std::move(list);
-	EXPECT_EQ(list.size(), 0);
-	EXPECT_EQ(list2.size(), 3);
-}
-
-TEST_F(test_resource_list, constructorByMovementshouldNotMakeCopies)
+TEST_F(test_resource_list, constructor_by_mov_should_not_copy)
 {
 	test_list.emplace_back(test_resource());
 	test_list.emplace_back(test_resource());
 	test_list.emplace_back(test_resource());
 
 	list<test_resource> resource_list = std::move(test_list);
-
 	EXPECT_EQ(test_resource::copy_constructor, 0);
+}
+
+TEST(constructor_by_mov, should_update_nelms)
+{
+	intlist list{ 1,2,3 };
+	const std::size_t list_current_size = list.size();
+	intlist list2 = std::move(list);;
+	EXPECT_EQ(list.size(), 0);
+	EXPECT_EQ(list2.size(), list_current_size);
 }
