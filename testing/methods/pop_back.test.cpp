1a4f15b6edd8652ca8c44148d4ddbe49799728d4
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-06 16:31:52 -0500

    feat: improve pop_back function tests readability

--- testing/methods/pop_back.test.cpp
+++ testing/methods/pop_back.test.cpp
@@ -1,28 +1,24 @@
 #include <gtest/gtest.h>
-#include "../helpers/intlist.typealias.helper.h"
+#include "intlist.typealias.helper.h"
 
-TEST(pop_back, shouldPoptheBack)
+TEST(pop_back, should_pop_back)
 {
-	intlist list;
-	list.push_back(1);
-	list.push_back(2);
-	list.push_back(3);
-
+	const int aux = 2;
+	intlist list{ aux, 3 };
 	list.pop_back();
-
-	EXPECT_EQ(list.back(), 2);
+	EXPECT_EQ(list.back(), aux);
 }
 
-TEST(pop_back, ifListEmptyShouldThrowException)
+TEST(pop_back, should_throw_length_error_if_list_empty)
 {
-	intlist emptylist;
-	EXPECT_THROW(emptylist.pop_back(), std::length_error);
+	intlist empty_list;
+	EXPECT_THROW(empty_list.pop_back(), std::length_error);
 }
 
-TEST(pop_back, shouldChangeSize)
+TEST(pop_back, should_update_nelms)
 {
 	intlist list{ 1,2,3 };
-	const auto currentSize = list.size();
+	const auto current_size = list.size();
 	list.pop_back();
-	EXPECT_EQ(list.size(), currentSize - 1);
+	EXPECT_EQ(list.size(), current_size - 1);
 }
