c77b7cc78a5b25bf396a903961668730ccc431ea
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-06 17:35:58 -0500

    feat: improve initializer list constructor tests

--- testing/constructors/initializer-list.test.cpp
+++ testing/constructors/initializer-list.test.cpp
@@ -1,8 +1,20 @@
 #include <gtest/gtest.h>
-#include "../helpers/intlist.typealias.helper.h"
+#include "intlist.typealias.helper.h"
+#include "compare-list.helper.h"
+#include "resource.helper.h"
 
-TEST(initializerListConstructor, shouldConstructTheList)
+TEST(initializer_list_constructor, should_construct_list_successfully)
 {
 	intlist list{ 1, 2, 3, 4, 5 };
-	EXPECT_EQ(list.size(), 5);
+	EXPECT_TRUE(compare_list(list, intlist{ 1,2,3,4,5 }));
+}
+
+TEST_F(test_resource_list, initializer_of_rvalues_should_not_copy)
+{
+	test_resource res1;
+	test_resource res2;
+	list<test_resource> list{ std::move(res1), std::move(res2) };
+
+	EXPECT_EQ(test_resource::instances_created, 2);
+	EXPECT_EQ(test_resource::movement_constructor, 2);
 }
