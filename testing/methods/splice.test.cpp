c33c84ecf8fb523bac3b5acdb6768799c1549758
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-06 17:01:34 -0500

    feat: improve splice function tests

--- testing/methods/splice.test.cpp
+++ testing/methods/splice.test.cpp
@@ -1,71 +1,47 @@
 #include <gtest/gtest.h>
 #include "list.h"
-#include "../helpers/intlist.typealias.helper.h"
-#include "../helpers/compare-list.helper.h"
+#include "intlist.typealias.helper.h"
+#include "compare-list.helper.h"
 
-TEST(splice, shouldSpliceSuccefully)
+TEST(splice, should_splice_succefully)
 {
 	intlist list{ 1,2,3 };
-	intlist splicedlist{ 9,8,7 };
-	auto begin = list.begin();
+	intlist spliced_list{ 9,8,7 };
 
-	list.splice(begin, splicedlist);
+	auto begin = list.begin();
+	list.splice(begin, spliced_list);
 
 	EXPECT_TRUE(compare_list(list, intlist{ 1,9,8,7,2,3 }));
 }
 
-TEST(splice, shouldUpdateNelms)
+TEST(splice, should_update_nelms_in_both_lists)
 {
 	intlist list{ 1,2,3 };
-	intlist splicedlist{ 9,8,7 };
-	auto begin = list.begin();
+	const std::size_t list_current_size = list.size();
 
-	list.splice(begin, splicedlist);
+	intlist spliced_list{ 9,8,7 };
+	const std::size_t spliced_list_size = spliced_list.size();
+	
+	list.splice(list.begin(), spliced_list);
 
-	EXPECT_EQ(list.size(), 6);
-	EXPECT_EQ(splicedlist.size(), 0);
+	EXPECT_EQ(list.size(), list_current_size + spliced_list_size);
+	EXPECT_EQ(spliced_list.size(), 0);
 }
 
-TEST(splice, rightListShouldBeEmtpyAfterSplice)
-{
-
-	intlist list{ 1,2,3 };
-	intlist splicedlist{ 9,8,7 };
-	auto begin = list.begin();
-
-	list.splice(begin, splicedlist);
-
-	EXPECT_TRUE(splicedlist.empty());
-}
-
-TEST(splice, ifRightIsEmpty)
+TEST(splice, should_not_throw_error_if_rhs_list_empty)
 {
 	intlist list{ 1,2,3 };
 	intlist empty;
 
-	list.splice(list.begin(), empty);
-
-	EXPECT_TRUE(compare_list(list, intlist{ 1,2,3 }));
-	EXPECT_TRUE(empty.empty());
+	ASSERT_NO_THROW(list.splice(list.begin(), empty));
+	EXPECT_TRUE(compare_list(list, intlist{ 1,2,3 }));	
 }
 
-TEST(splice, ifCurrentListIsEmpty)
+TEST(splice, should_not_throw_error_if_current_list_empty)
 {
 	intlist list;
-	intlist newlist{ 1,2,3 };
+	intlist new_list{ 1,2,3 };
 
-	list.splice(list.end(), newlist);
-
+	ASSERT_NO_THROW(list.splice(list.begin(), new_list));
 	EXPECT_TRUE(compare_list(list, intlist{ 1,2,3 }));
-	EXPECT_TRUE(newlist.empty());
-}
-
-TEST(splice, shouldAcceptDifferentTypesOfIterator)
-{
-	intlist list;
-	intlist testlist;
-
-	list.splice(list.cbegin(), testlist);
-	list.splice(list.rbegin(), testlist);
-	list.splice(list.crbegin(), testlist);
 }
