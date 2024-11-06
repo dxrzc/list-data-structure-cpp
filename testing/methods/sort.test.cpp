d071f389b1ff92270c3e34eea359b57ebf51a65e
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-06 16:56:06 -0500

    feat: improve sort function tests readability

--- testing/methods/sort.test.cpp
+++ testing/methods/sort.test.cpp
@@ -1,33 +1,25 @@
 #include <gtest/gtest.h>
 #include "list.h"
-#include "../helpers/intlist.typealias.helper.h"
-#include "../helpers/compare-list.helper.h"
+#include "intlist.typealias.helper.h"
+#include "compare-list.helper.h"
 
-TEST(sort, shouldSortListSuccefully)
+TEST(sort, should_sort_list_successfully)
 {
 	intlist list{ 0,6,2,3,9,7,1,4,5,8 };
 	list.sort();
-	EXPECT_TRUE(compare_list(list, intlist{ 0,1,2,3,4,5,6,7,8,9 }));
-	EXPECT_EQ(list.size(), 10);
+	EXPECT_TRUE(compare_list(list, intlist{ 0,1,2,3,4,5,6,7,8,9 }));	
 }
 
-TEST(sort, shouldSortListSuccefully2)
-{
-	intlist list{ 3,2 };
-	list.sort();
-	EXPECT_TRUE(compare_list(list, intlist{ 2,3 }));
-}
-
-TEST(sort, ifListEmpty)
+TEST(sort, should_not_throw_error_if_list_empty)
 {
 	intlist emptylist;
-	emptylist.sort();
+	ASSERT_NO_THROW(emptylist.sort());
 	EXPECT_TRUE(emptylist.empty());
 }
 
-TEST(sort, ifSizeIsOne)
+TEST(sort, should_not_throw_error_if_list_has_one_elm)
 {
 	intlist list{ 1 };
-	list.sort();
+	ASSERT_NO_THROW(list.sort());
 	EXPECT_TRUE(compare_list(list, intlist{ 1 }));
 }
