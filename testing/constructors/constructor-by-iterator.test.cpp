aae1c31689bbe5ef528281729de30b4e988ff6f5
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-06 17:15:48 -0500

    feat: improve constructor by iterators tests readability

--- testing/constructors/constructor-by-iterator.test.cpp
+++ testing/constructors/constructor-by-iterator.test.cpp
@@ -1,30 +1,19 @@
 #include <gtest/gtest.h>
-#include "../helpers/intlist.typealias.helper.h"
-#include "../helpers/compare-list.helper.h"
+#include "intlist.typealias.helper.h"
+#include "compare-list.helper.h"
 
-TEST(constructorUsingIterator, shouldCreateTheListSuccefully)
+TEST(constructor_by_iterators, should_create_list_successfully)
 {
 	intlist list{ 1,2,3,4,5 };
-	intlist::iterator begin = ++(list.begin());
-	intlist::iterator end = --(list.end());
+	intlist::iterator begin = ++(list.begin()); // 2
+	intlist::iterator end = --(list.end()); // 5
 
 	intlist newlist(begin, end);
 	EXPECT_TRUE(compare_list(newlist, intlist{ 2,3,4 }));
 	EXPECT_EQ(newlist.size(), 3);
 }
 
-TEST(constructorUsingConst_Iterator, shouldCreateTheListSuccefully)
-{
-	intlist list{ 1,2,3,4,5 };
-	intlist::const_iterator cbegin = ++(list.cbegin());
-	intlist::const_iterator cend = --(list.cend());
-
-	intlist newlist(cbegin, cend);
-	EXPECT_TRUE(compare_list(newlist, intlist{ 2,3,4 }));
-	EXPECT_EQ(newlist.size(), 3);
-}
-
-TEST(constructorUsingIterator, ifListIsEmpty)
+TEST(constructor_by_iterators, should_success_if_list_empty)
 {
 	intlist emptylist;
 	intlist newlist(emptylist.begin(), emptylist.end());
