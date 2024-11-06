b3c16309dc8776b1610aea56aa61c4b95bb79ed0
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-06 13:15:21 -0500

    chore: rename emplace-it.test.cpp to emplace.test.cpp

--- testing/methods/emplace.test.cpp
+++ testing/methods/emplace.test.cpp
@@ -10,13 +10,13 @@ TEST(emplace_by_iterator, should_insert_successfully)
 	EXPECT_EQ(list.front(), new_value);
 }
 
-TEST_F(test_resource_list, should_not_copy)
+TEST_F(test_resource_list, emplace_by_iterator_should_not_copy)
 {
 	test_list.emplace(test_list.end(), test_resource());
 	EXPECT_EQ(test_resource::instances_created, 1);
 }
 
-TEST(emplaceUsingIterator, shouldUpdateNelms)
+TEST(emplace_by_iterator, should_update_nelms)
 {
 	intlist list{ 1,2,3 };
 	list.emplace(list.end(), 10);
