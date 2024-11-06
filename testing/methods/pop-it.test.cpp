d9b89ebcddadb719a833517db7b267a8b672e1a4
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-06 16:29:53 -0500

    feat: improve pop function tests

--- testing/methods/pop-it.test.cpp
+++ testing/methods/pop-it.test.cpp
@@ -1,37 +1,39 @@
 #include <gtest/gtest.h>
 #include "../helpers/intlist.typealias.helper.h"
 
-TEST(popUsingIterator, shouldDeleteTheElementAtIterator)
+TEST(pop_by_iterator, should_pop_element_succesfully)
 {
-	intlist list{ 1, 2, 3 };
+	const int aux = 2;
+	intlist list{ 1, aux, 3 };
 	list.pop(list.begin());
-	EXPECT_EQ(list.front(), 2);
+	EXPECT_EQ(list.front(), aux);
 }
 
-TEST(popUsingIterator, shouldReturnIteratorToNextElement)
+TEST(pop_by_iterator, should_return_iterator_to_next_element)
 {
-	intlist list{ 1, 2, 3 };
-	auto it = list.begin();
-	auto nextElement = list.pop(it);
-	EXPECT_EQ(*nextElement, 2);
+	const int aux = 2;
+	intlist list{ 1, aux, 3 };
+	auto it = list.pop(list.begin());
+	EXPECT_EQ(*it, aux);
 }
 
-TEST(popUsingIterator, shouldDecrementNelms)
+TEST(pop_by_iterator, should_update_nelms)
 {
 	intlist list{ 1, 2, 3 };
+	const std::size_t current_size = list.size();
 	list.pop(list.begin());
-	EXPECT_EQ(list.size(), 2);
+	EXPECT_EQ(list.size(), current_size - 1);
 }
 
-TEST(popUsingIterator, shouldThrowLengthErrorIfListEmpty)
+TEST(pop_by_iterator, should_throw_length_error_if_list_empty)
 {
 	intlist list;
-	EXPECT_THROW(list.pop(list.begin()), std::length_error);
+	EXPECT_THROW(list.pop(list.begin()), std::length_error);	
 }
 
-TEST(popUsingIterator, shouldThrowExceptionIfIteratorPointsToHead)
+TEST(pop_by_iterator, should_throw_error_if_iterator_points_to_head)
 {
 	intlist list{ 1, 2 };
-	auto it = list.end();
+	auto it = list.end(); // pointing to head
 	EXPECT_THROW(list.pop(it), std::runtime_error);
 }
