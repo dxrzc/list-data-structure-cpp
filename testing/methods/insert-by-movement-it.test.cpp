a096e3603fac2253bcdc7679fc6e5bd8f1379352
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-06 14:13:12 -0500

    feat: improve insert by movement function tests

--- testing/methods/insert-by-movement-it.test.cpp
+++ testing/methods/insert-by-movement-it.test.cpp
@@ -1,24 +1,26 @@
 #include <gtest/gtest.h>
-#include "../helpers/intlist.typealias.helper.h"
-#include "../helpers/resource.helper.h"
+#include "intlist.typealias.helper.h"
+#include "resource.helper.h"
 
-TEST(insertUsingIteratorByMovement, shouldInsertBeforeTheIteratorGiven)
+TEST(insert_by_movement, should_insert_successfully)
 {
-	int newvalue = 100;
+	int new_value = 100;
 	intlist list{ 1,2,3 };
-	list.insert(list.begin(), std::move(newvalue));
-	EXPECT_EQ(list.front(), newvalue);
+	list.insert(list.begin(), std::move(new_value));
+	EXPECT_EQ(list.front(), new_value);
 }
 
-TEST_F(test_resource_list, insertByMovementUsingIteratorShouldNotCopy)
+TEST_F(test_resource_list, insert_by_mov_should_not_copy)
 {
-	test_list.insert(test_list.end(), test_resource());
+	test_resource res;
+	test_list.insert(test_list.end(), std::move(res));
 	EXPECT_EQ(test_resource::instances_created, 1);
 }
 
-TEST(insertUsingIteratorByMovement, shouldUpdateNelms)
+TEST(insert_by_movement, should_update_nelms)
 {
 	intlist list{ 1,2,3 };
+	const std::size_t current_size = list.size();
 	list.insert(list.end(), 10);
-	EXPECT_EQ(list.size(), 4);
+	EXPECT_EQ(list.size(), current_size + 1);
 }
