ac63db796a934c7098e1d376e4a5063bbc5c957e
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-06 17:07:16 -0500

    feat: improve const_reverse_iterator class tests readability

--- testing/iterators/const_reverse_iterator.test.cpp
+++ testing/iterators/const_reverse_iterator.test.cpp
@@ -1,162 +1,161 @@
 #include <gtest/gtest.h>
-#include "../helpers/intlist.typealias.helper.h"
+#include "intlist.typealias.helper.h"
 
-TEST(const_reverse_iterator, crbeginShouldReturnIteratorToLastElement)
+TEST(const_reverse_iterator, crbegin_should_return_iterator_to_last_element)
 {
-	const int target = 3;
-	intlist list{ 1,2,target };
-	intlist::const_reverse_iterator crevit = list.crbegin();
-	EXPECT_EQ(target, *crevit);
+    const int target = 3;
+    intlist list{ 1, 2, target };
+    intlist::const_reverse_iterator crevit = list.crbegin();
+    EXPECT_EQ(target, *crevit);
 }
 
-TEST(const_reverse_iterator, crendShouldReturnIteratorToHead)
+TEST(const_reverse_iterator, crend_should_return_iterator_to_head)
 {
-	intlist list;
-	EXPECT_ANY_THROW(*(list.crend()));
+    intlist list;
+    EXPECT_ANY_THROW(*(list.crend()));
 }
 
-TEST(const_reverse_iterator, preIncrementOperator)
+TEST(const_reverse_iterator, pre_increment_operator)
 {
-	intlist list{ 1,2,3 };
-	auto crevit = list.crbegin();
-	EXPECT_EQ(*(++crevit), 2);
-	EXPECT_EQ(*(++crevit), 1);
+    intlist list{ 1, 2, 3 };
+    auto crevit = list.crbegin();
+    EXPECT_EQ(*(++crevit), 2);
+    EXPECT_EQ(*(++crevit), 1);
 }
 
-TEST(const_reverse_iterator, postIncrementOperator)
+TEST(const_reverse_iterator, post_increment_operator)
 {
-	intlist list{ 1,2,3 };
-	auto crevit = list.crbegin();
-	EXPECT_EQ(*(crevit++), 3);
-	EXPECT_EQ(*(crevit++), 2);
-	EXPECT_EQ(*(crevit++), 1);
+    intlist list{ 1, 2, 3 };
+    auto crevit = list.crbegin();
+    EXPECT_EQ(*(crevit++), 3);
+    EXPECT_EQ(*(crevit++), 2);
+    EXPECT_EQ(*(crevit++), 1);
 }
 
-TEST(const_reverse_iterator, preDecrementOperator)
+TEST(const_reverse_iterator, pre_decrement_operator)
 {
-	intlist list{ 1,2,3 };
-	auto crevit = list.crend();
-	EXPECT_EQ(*(--crevit), 1);
-	EXPECT_EQ(*(--crevit), 2);
-	EXPECT_EQ(*(--crevit), 3);
+    intlist list{ 1, 2, 3 };
+    auto crevit = list.crend();
+    EXPECT_EQ(*(--crevit), 1);
+    EXPECT_EQ(*(--crevit), 2);
+    EXPECT_EQ(*(--crevit), 3);
 }
 
-TEST(const_reverse_iterator, postDecrementOperator)
+TEST(const_reverse_iterator, post_decrement_operator)
 {
-	intlist list{ 1,2,3 };
-	auto crevit = list.crend();
-	crevit--;
-	EXPECT_EQ(*(crevit--), 1);
-	EXPECT_EQ(*(crevit--), 2);
-	EXPECT_EQ(*(crevit--), 3);
+    intlist list{ 1, 2, 3 };
+    auto crevit = list.crend();
+    crevit--;
+    EXPECT_EQ(*(crevit--), 1);
+    EXPECT_EQ(*(crevit--), 2);
+    EXPECT_EQ(*(crevit--), 3);
 }
 
-
-TEST(const_reverse_iterator, shouldThrowExceptionIfTriesToGetHeadValue)
+TEST(const_reverse_iterator, should_throw_exception_if_tries_to_get_head_value)
 {
-	intlist list;
-	auto crevit = list.rend();
-	EXPECT_ANY_THROW(*crevit);
+    intlist list;
+    auto crevit = list.rend();
+    EXPECT_ANY_THROW(*crevit);
 }
 
-TEST(const_reverse_iterator, shouldBeAbleToUseStdAdvance)
+TEST(const_reverse_iterator, should_be_able_to_use_std_advance)
 {
-	const int target = 100;
-	intlist list{ target,2,3 };
-	auto crevit = list.crbegin();
-	std::advance(crevit, 2);
-	EXPECT_EQ(*crevit, target);
+    const int target = 100;
+    intlist list{ target, 2, 3 };
+    auto crevit = list.crbegin();
+    std::advance(crevit, 2);
+    EXPECT_EQ(*crevit, target);
 }
 
-TEST(const_reverse_iterator, equalityOperator)
+TEST(const_reverse_iterator, equality_operator)
 {
-	const int target = 100;
-	intlist list{ 1,2,target };
-	auto crevit1 = list.crbegin();
-	auto crevit2 = list.crbegin();
-	EXPECT_TRUE(crevit1 == crevit2);
+    const int target = 100;
+    intlist list{ 1, 2, target };
+    auto crevit1 = list.crbegin();
+    auto crevit2 = list.crbegin();
+    EXPECT_TRUE(crevit1 == crevit2);
 }
 
-TEST(const_reverse_iterator, inequalityOperator)
+TEST(const_reverse_iterator, inequality_operator)
 {
-	const int target = 100;
-	intlist list{ 1,2,target };
-	auto crevit1 = list.crbegin();
-	auto crevit2 = list.crbegin();
-	++crevit2;
-	EXPECT_TRUE(crevit1 != crevit2);
+    const int target = 100;
+    intlist list{ 1, 2, target };
+    auto crevit1 = list.crbegin();
+    auto crevit2 = list.crbegin();
+    ++crevit2;
+    EXPECT_TRUE(crevit1 != crevit2);
 }
 
-TEST(const_reverse_iterator, copyConstructor)
+TEST(const_reverse_iterator, copy_constructor)
 {
-	intlist list{ 1,2,3 };
-	intlist::const_reverse_iterator crevit = list.crbegin();
-	intlist::const_reverse_iterator crevit2 = crevit;
-	EXPECT_TRUE(crevit == crevit2);
+    intlist list{ 1, 2, 3 };
+    intlist::const_reverse_iterator crevit = list.crbegin();
+    intlist::const_reverse_iterator crevit2 = crevit;
+    EXPECT_TRUE(crevit == crevit2);
 }
 
-TEST(const_reverse_iterator, equalOperator)
+TEST(const_reverse_iterator, equal_operator)
 {
-	intlist list{ 1,2,3 };
-	auto crevit1 = list.crbegin();
-	auto crevit2 = list.crbegin();
-	++crevit1;
-	crevit2 = crevit1;
-	EXPECT_TRUE(crevit2 == crevit1);
+    intlist list{ 1, 2, 3 };
+    auto crevit1 = list.crbegin();
+    auto crevit2 = list.crbegin();
+    ++crevit1;
+    crevit2 = crevit1;
+    EXPECT_TRUE(crevit2 == crevit1);
 }
 
-TEST(const_reverse_iterator, desreferenceShouldReturnTheCorrectValue)
+TEST(const_reverse_iterator, dereference_should_return_the_correct_value)
 {
-	const int target = 3;
-	intlist list{ 1,2,target };
-	auto crevit = list.crbegin();
-	EXPECT_EQ(*crevit, target);
+    const int target = 3;
+    intlist list{ 1, 2, target };
+    auto crevit = list.crbegin();
+    EXPECT_EQ(*crevit, target);
 }
 
-TEST(const_reverse_iterator, shouldBeConstructedUsingAnIterator)
+TEST(const_reverse_iterator, should_be_constructed_using_an_iterator_successfully)
 {
-	intlist list{ 1,2,3 };
-	auto it = list.begin();
-	intlist::const_reverse_iterator crevit = it;
-	EXPECT_EQ(*crevit, *it);
+    intlist list{ 1, 2, 3 };
+    auto it = list.begin();
+    intlist::const_reverse_iterator crevit = it;
+    EXPECT_EQ(*crevit, *it);
 }
 
-TEST(const_reverse_iterator, shouldBeConstructedUsingAConstIterator)
+TEST(const_reverse_iterator, should_be_constructed_using_a_const_iterator_successfully)
 {
-	intlist list{ 1,2,3 };
-	auto it = list.cbegin();
-	intlist::const_reverse_iterator crevit = it;
-	EXPECT_EQ(*crevit, *it);
+    intlist list{ 1, 2, 3 };
+    auto it = list.cbegin();
+    intlist::const_reverse_iterator crevit = it;
+    EXPECT_EQ(*crevit, *it);
 }
 
-TEST(const_reverse_iterator, shouldBeConstructedUsingAReverseIterator)
+TEST(const_reverse_iterator, should_be_constructed_using_a_reverse_iterator_successfully)
 {
-	intlist list{ 1,2,3 };
-	auto it = list.rbegin();
-	intlist::const_reverse_iterator crevit = it;
-	EXPECT_EQ(*crevit, *it);
+    intlist list{ 1, 2, 3 };
+    auto it = list.rbegin();
+    intlist::const_reverse_iterator crevit = it;
+    EXPECT_EQ(*crevit, *it);
 }
 
-TEST(const_reverse_iterator, equalityOperatorWithIterator)
+TEST(const_reverse_iterator, equality_operator_with_iterator)
 {
-	intlist list{ 1,2,3 };
-	intlist::const_reverse_iterator crevit = list.crbegin();
-	intlist::iterator it = --(list.end());
-	EXPECT_TRUE(crevit == it);
+    intlist list{ 1, 2, 3 };
+    intlist::const_reverse_iterator crevit = list.crbegin();
+    intlist::iterator it = --(list.end());
+    EXPECT_TRUE(crevit == it);
 }
 
-TEST(const_reverse_iterator, equalityOperatorWithConstIterator)
+TEST(const_reverse_iterator, equality_operator_with_const_iterator)
 {
-	intlist list{ 1,2,3 };
-	intlist::const_reverse_iterator crevit = list.crbegin();
-	intlist::const_iterator cit = --(list.cend());
-	EXPECT_TRUE(crevit == cit);
+    intlist list{ 1, 2, 3 };
+    intlist::const_reverse_iterator crevit = list.crbegin();
+    intlist::const_iterator cit = --(list.cend());
+    EXPECT_TRUE(crevit == cit);
 }
 
-TEST(const_reverse_iterator, equalityOperatorWithReverseIterator)
+TEST(const_reverse_iterator, equality_operator_with_reverse_iterator)
 {
-	intlist list{ 1,2,3 };
-	intlist::const_reverse_iterator crevit = list.crbegin();
-	intlist::reverse_iterator revit = list.rbegin();
-	EXPECT_TRUE(crevit == revit);
+    intlist list{ 1, 2, 3 };
+    intlist::const_reverse_iterator crevit = list.crbegin();
+    intlist::reverse_iterator revit = list.rbegin();
+    EXPECT_TRUE(crevit == revit);
 }
