aa57a8d2b1d3c85a30240977b1228129ebbb95c0
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-13 13:45:51 -0500

    feat: update clear function tests to consider fatal failures

--- testing/methods/clear.test.cpp
+++ testing/methods/clear.test.cpp
@@ -9,12 +9,11 @@ TEST(clear, should_clear_the_list_succesfully)
 	EXPECT_TRUE(list.empty());
 }
 
-TEST(clear, should_not_throw_an_error_if_clear_is_called_two_times)
+TEST(clear, should_not_cause_fatal_failure_if_its_called_two_times)
 {
 	intlist list{ 1,2,3 };
-	ASSERT_NO_THROW(list.clear());
-	ASSERT_NO_THROW(list.clear());
-	EXPECT_TRUE(true);
+	list.clear();
+	EXPECT_NO_FATAL_FAILURE(list.clear());	
 }
 
 TEST(clear, nelms_should_be_zero_after_call)
@@ -24,18 +23,18 @@ TEST(clear, nelms_should_be_zero_after_call)
 	EXPECT_EQ(list.size(), 0);
 }
 
-TEST(clear, should_not_throw_error_if_list_empty)
+TEST(clear, should_not_cause_fatal_failure_on_empty_list)
 {
 	intlist list;
-	ASSERT_NO_THROW(list.clear());
+	EXPECT_NO_FATAL_FAILURE(list.clear());
 }
 
 TEST(clear, should_be_able_to_add_elements_after_call)
 {
 	intlist list{ 1, 2, 3 };
 	list.clear();
-	ASSERT_NO_THROW(list.push_back(0));
-	ASSERT_NO_THROW(list.push_back(1));
-	ASSERT_NO_THROW(list.push_back(0));
+	EXPECT_NO_FATAL_FAILURE(list.push_back(0));
+	EXPECT_NO_FATAL_FAILURE(list.push_back(1));
+	EXPECT_NO_FATAL_FAILURE(list.push_back(0));
 	EXPECT_TRUE(compare_list(list, { 0, 1, 0 }));
 }
