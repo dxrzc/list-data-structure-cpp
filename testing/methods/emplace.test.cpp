63f0491eef0dda5d965e5e763f0dd1e020e2bd6b
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-23 12:46:42 -0500

    feat: update emplace and insert functions tests to make a full comparison between lists

--- testing/methods/emplace.test.cpp
+++ testing/methods/emplace.test.cpp
@@ -1,13 +1,41 @@
 #include <gtest/gtest.h>
 #include "intlist.typealias.helper.h"
+#include "compare-list.helper.h"
 #include "resource.helper.h"
 
-TEST(emplace_by_iterator, should_insert_successfully)
+TEST(emplace_by_iterator, should_insert_succesfully_at_begin)
 {
-	const int new_value = 100;
+	const int new_value = 0;
+	intlist expected_list{ new_value, 1,2,3 };
+
 	intlist list{ 1,2,3 };
 	list.emplace(list.begin(), new_value);
-	EXPECT_EQ(list.front(), new_value);
+
+	EXPECT_TRUE(compare_list(list, expected_list));
+}
+
+TEST(emplace_by_iterator, should_insert_sucesfully_at_end)
+{
+	const int new_value = 4;
+	intlist expected_list{ 1,2,3,4 };
+
+	intlist list{ 1,2,3 };
+	list.emplace(list.end(), new_value);
+
+	EXPECT_TRUE(compare_list(list, expected_list));
+}
+
+TEST(emplace_by_iterator, should_insert_sucesfully_at_random_pos)
+{
+	const int new_value = 3;
+	intlist expected_list{ 1,2,new_value,4,5 };
+
+	intlist list{ 1,2,4,5 };
+	intlist::iterator it = list.begin();
+	std::advance(it, 2);
+	list.emplace(it, new_value);
+
+	EXPECT_TRUE(compare_list(list, expected_list));
 }
 
 TEST_F(test_resource_list, emplace_by_iterator_should_not_copy)
--- testing/methods/insert.test.cpp
+++ testing/methods/insert.test.cpp
@@ -1,12 +1,40 @@
 #include <gtest/gtest.h>
 #include "intlist.typealias.helper.h"
+#include "compare-list.helper.h"
 
-TEST(insert_by_iterator, should_insert_succesfully)
+TEST(insert_by_iterator, should_insert_succesfully_at_begin)
 {
-	intlist list{ 1,2,3 };
 	const int new_value = 0;
-	list.insert(list.begin(), new_value);
-	EXPECT_EQ(list.front(), new_value);
+	intlist expected_list = { new_value ,1,2,3 };
+
+	intlist list{ 1,2,3 };
+	EXPECT_NO_FATAL_FAILURE(list.insert(list.begin(), new_value));	
+
+	EXPECT_TRUE(compare_list(list, expected_list));
+}
+
+TEST(insert_by_iterator, should_insert_succesfully_at_end)
+{
+	const int new_value = 4;
+	intlist expected_list = { 1,2,3, new_value };
+
+	intlist list{ 1,2,3 };
+	EXPECT_NO_FATAL_FAILURE(list.insert(list.end(), new_value));
+
+	EXPECT_TRUE(compare_list(list, expected_list));
+}
+
+TEST(insert_by_iterator, should_insert_succesfully_at_random_pos)
+{
+	const int new_value = 100;
+	intlist expected_list = { 1,2,100,3 };
+
+	intlist list{ 1,2,3 };
+	intlist::iterator it = list.begin();
+	std::advance(it, 2);
+	list.insert(it, new_value);
+
+	EXPECT_TRUE(compare_list(list, expected_list));
 }
 
 TEST(insert_by_iterator, should_return_iterator_to_new_value)
