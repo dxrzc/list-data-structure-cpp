eea1c65ede6493b76fbafdfec93f708294ea4e5b
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-12-25 14:35:16 -0500

    feat: enhance copy constructor tests coverage

--- testing/constructors/copy-constructor.test.cpp
+++ testing/constructors/copy-constructor.test.cpp
@@ -5,13 +5,23 @@
 TEST(copy_constructor, should_copy_successfully)
 {
 	intlist list{ 1,2,3 };
-	auto new_list(list);
-	EXPECT_TRUE(compare_list(list, new_list));
+	intlist test_list(list);
+
+	EXPECT_TRUE(compare_list(test_list, list));
 }
 
 TEST(copy_constructor, should_update_nelms)
 {
-	intlist list{ 1, 2, 3, 4, 5 };	
-	intlist list2 = list;
-	EXPECT_EQ(list.size(), list2.size());
+	intlist list{ 1,2,3 };
+	intlist test_list(list);
+
+	EXPECT_EQ(list.size(), test_list.size());
+}
+
+TEST(copy_constructor, should_not_make_changes_on_rhs_list)
+{
+	intlist list{ 1,2,3 };
+	intlist test_list(list);
+
+	EXPECT_TRUE(compare_list(list, intlist{ 1,2,3 }));
 }
