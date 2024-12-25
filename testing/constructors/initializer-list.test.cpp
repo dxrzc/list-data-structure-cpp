c735c6aff7bbfae7c9805111a8baea113fe6100c
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-12-25 14:55:42 -0500

    chore: remove useless test for initializer list

--- testing/constructors/initializer-list.test.cpp
+++ testing/constructors/initializer-list.test.cpp
@@ -8,13 +8,3 @@ TEST(initializer_list_constructor, should_construct_list_successfully)
 	intlist list{ 1, 2, 3, 4, 5 };
 	EXPECT_TRUE(compare_list(list, intlist{ 1,2,3,4,5 }));
 }
-
-TEST_F(test_resource_list, initializer_of_rvalues_should_not_copy)
-{
-	test_resource res1;
-	test_resource res2;
-	list<test_resource> list{ std::move(res1), std::move(res2) };
-
-	EXPECT_EQ(test_resource::instances_created, 2);
-	EXPECT_EQ(test_resource::movement_constructor, 2);
-}
