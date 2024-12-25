f28003ba1c72bbb632a1d6ddd31b5c275c0e8442
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-12-25 14:12:13 -0500

    feat: enhance constructor by mov tests coverage

--- testing/constructors/constructor-by-movement.test.cpp
+++ testing/constructors/constructor-by-movement.test.cpp
@@ -3,20 +3,33 @@
 #include "resource.helper.h"
 
 TEST_F(test_resource_list, constructor_by_mov_should_not_copy)
-{
-	test_list.emplace_back(test_resource());
-	test_list.emplace_back(test_resource());
-	test_list.emplace_back(test_resource());
+{	
+	list<test_resource> test_resource_list;
+	// rvalue forwarding makes no copies
+	test_resource_list.push_back(test_resource());
+	test_resource_list.push_back(test_resource());
+	test_resource_list.push_back(test_resource());
 
-	list<test_resource> resource_list = std::move(test_list);
+	list<test_resource> tested_list(std::move(test_resource_list));
+
 	EXPECT_EQ(test_resource::copy_constructor, 0);
+}
+
+TEST(constructor_by_mov, rhs_list_should_be_empty)
+{
+	intlist test_list{ 1,2,3 };
+	intlist tested_list (std::move(test_list));
+
+	EXPECT_TRUE(test_list.empty());
 }
 
 TEST(constructor_by_mov, should_update_nelms)
 {
 	intlist list{ 1,2,3 };
 	const std::size_t list_current_size = list.size();
+
 	intlist list2 = std::move(list);;
+
 	EXPECT_EQ(list.size(), 0);
 	EXPECT_EQ(list2.size(), list_current_size);
 }
