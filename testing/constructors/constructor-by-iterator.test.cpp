ad3c9739be8ff4217cfea64da7a101db47cedb0e
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-12-25 14:30:25 -0500

    feat: enhance constructor by iterators tests coverage

--- testing/constructors/constructor-by-iterator.test.cpp
+++ testing/constructors/constructor-by-iterator.test.cpp
@@ -8,14 +8,31 @@ TEST(constructor_by_iterators, should_create_list_successfully)
 	intlist::iterator begin = ++(list.begin()); // 2
 	intlist::iterator end = --(list.end()); // 5
 
-	intlist newlist(begin, end);
-	EXPECT_TRUE(compare_list(newlist, intlist{ 2,3,4 }));
-	EXPECT_EQ(newlist.size(), 3);
+	intlist test_list(begin, end);
+
+	EXPECT_TRUE(compare_list(test_list, intlist{ 2,3,4 }));
 }
 
-TEST(constructor_by_iterators, should_success_if_list_empty)
+TEST(constructor_by_iterators, should_create_list_successfully_using_begin_and_end_iterators)
 {
-	intlist emptylist;
-	intlist newlist(emptylist.begin(), emptylist.end());
-	EXPECT_TRUE(compare_list(emptylist, newlist));
+	intlist list{ 1,2,3,4,5 };	
+	intlist test_list(list.begin(), list.end());
+
+	EXPECT_TRUE(compare_list(test_list, list));
+}
+
+TEST(constructor_by_iterators, should_success_if_rhs_list_empty)
+{
+	intlist rhs_list;
+	intlist newlist(rhs_list.begin(), rhs_list.end());
+
+	EXPECT_TRUE(compare_list(rhs_list, newlist));
+}
+
+TEST(constructor_by_iterators, should_not_make_changes_on_rhs_list)
+{
+	intlist rhs_list{ 1,2,3 };
+	intlist test_list(++rhs_list.begin(), rhs_list.end());
+
+	EXPECT_TRUE(compare_list(rhs_list, intlist{ 1,2,3 }));
 }
