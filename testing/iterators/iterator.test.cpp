09606d3d52c89efd7d9d45694f0e109b58964d3a
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-05-06 11:03:25 -0500

    test: iterator equality operator tests

--- testing/iterators/iterator.test.cpp
+++ testing/iterators/iterator.test.cpp
@@ -25,9 +25,58 @@ TEST(iterator, successful_construction_based_on_reverse_iterator)
     intlist::reverse_iterator revit = list.rbegin();
     std::advance(revit, 2);
 
-    intlist::iterator it(revit);
+    intlist::iterator it = revit;
 
     EXPECT_EQ(*it , *revit);
+}
+
+TEST(iterator, equality_operator_works_with_other_iterators)
+{
+    intlist list{ 1,2,3,4,5 };
+
+    intlist::iterator another_iterator = list.begin();
+    std::advance(another_iterator, 2);
+
+    intlist::iterator it = list.begin();
+    std::advance(it, 2);
+
+    EXPECT_TRUE(it == another_iterator);
+}
+
+
+TEST(iterator, equality_operator_works_with_reverse_iterator)
+{
+    intlist list{ 1,2,3,4,5 };
+
+    intlist::reverse_iterator revit = list.rbegin();
+    
+    intlist::iterator it = list.begin();
+    std::advance(it, 4);
+
+    EXPECT_TRUE(it == revit);
+}
+
+TEST(iterator, equality_operator_works_with_const_iterator)
+{
+    intlist list{ 1,2,3,4,5 };
+
+    intlist::const_iterator cit = list.cbegin();
+
+    intlist::iterator it = list.begin();    
+
+    EXPECT_TRUE(it == cit);
+}
+
+TEST(iterator, equality_operator_works_with_const_reverse_iterator)
+{
+    intlist list{ 1,2,3,4,5 };
+
+    intlist::const_reverse_iterator cit = list.crbegin();
+
+    intlist::iterator it = list.begin();
+    std::advance(it, 4);
+
+    EXPECT_TRUE(it == cit);
 }
 
 TEST(iterator, begin_should_return_iterator_to_begin)
@@ -91,23 +140,6 @@ TEST(iterator, should_be_able_to_use_std_advance)
     auto it = list.begin();
     std::advance(it, 2);
     EXPECT_EQ(*it, 3);
-}
-
-TEST(iterator, equality_operator)
-{
-    intlist list{ 1, 2, 3 };
-    auto it1 = list.begin();
-    auto it2 = list.begin();
-    EXPECT_TRUE(it1 == it2);
-}
-
-TEST(iterator, inequality_operator)
-{
-    intlist list{ 1, 2, 3 };
-    auto it1 = list.begin();
-    auto it2 = list.begin();
-    ++it2;
-    EXPECT_FALSE(it1 == it2);
 }
 
 TEST(iterator, copy_constructor)
