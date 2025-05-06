811018efd7ba89be3f0709d973c008075a2bc563
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-05-06 11:19:06 -0500

    test: iterator equal operator tests

--- testing/iterators/iterator.test.cpp
+++ testing/iterators/iterator.test.cpp
@@ -43,7 +43,6 @@ TEST(iterator, equality_operator_works_with_other_iterators)
     EXPECT_TRUE(it == another_iterator);
 }
 
-
 TEST(iterator, equality_operator_works_with_reverse_iterator)
 {
     intlist list{ 1,2,3,4,5 };
@@ -79,52 +78,31 @@ TEST(iterator, equality_operator_works_with_const_reverse_iterator)
     EXPECT_TRUE(it == cit);
 }
 
-TEST(iterator, begin_should_return_iterator_to_begin)
+TEST(iterator, equal_operator_works_with_another_iterator)
 {
-    const int begin = 1;
-    intlist list{ begin, 2, 3 };
-    EXPECT_EQ(*(list.begin()), begin);
+    intlist list{ 1,2,3,4,5 };
+
+    intlist::iterator another_iterator = list.begin();
+    std::advance(another_iterator, 3);
+
+    intlist::iterator it = list.begin();
+
+    it = another_iterator;
+
+    EXPECT_EQ(*it, *another_iterator);
 }
 
-TEST(iterator, end_should_return_iterator_to_end)
+TEST(iterator, equal_operator_works_with_reverse_iterator)
 {
-    intlist list{ 1, 2, 3 };
-    EXPECT_ANY_THROW(*(list.end()));
-}
+    intlist list{ 1,2,3,4,5 };
 
-TEST(iterator, pre_increment_operator)
-{
-    intlist list{ 1, 2, 3 };
-    auto it = list.begin();
-    EXPECT_EQ(*(++it), 2);
-    EXPECT_EQ(*(++it), 3);
-}
+    intlist::reverse_iterator revit = list.rbegin();
 
-TEST(iterator, post_increment_operator)
-{
-    intlist list{ 1, 2, 3 };
-    auto it = list.begin();
-    EXPECT_EQ(*(it++), 1);
-    EXPECT_EQ(*(it++), 2);
-    EXPECT_EQ(*(it++), 3);
-}
+    intlist::iterator it = list.begin();
 
-TEST(iterator, pre_decrement_operator)
-{
-    intlist list{ 1, 2, 3 };
-    auto it = list.end();
-    EXPECT_EQ(*(--it), 3);
-    EXPECT_EQ(*(--it), 2);
-}
+    it = revit;
 
-TEST(iterator, post_decrement_operator)
-{
-    intlist list{ 1, 2, 3 };
-    auto it = list.end();
-    it--;
-    EXPECT_EQ(*(it--), 3);
-    EXPECT_EQ(*(it--), 2);
-    EXPECT_EQ(*(it--), 1);
+    EXPECT_EQ(*it, *revit);
 }
 
 TEST(iterator, should_throw_exception_if_tries_to_get_head_value)
@@ -134,37 +112,66 @@ TEST(iterator, should_throw_exception_if_tries_to_get_head_value)
     EXPECT_THROW(*it, std::runtime_error);
 }
 
-TEST(iterator, should_be_able_to_use_std_advance)
-{
-    intlist list{ 1, 2, 3 };
-    auto it = list.begin();
-    std::advance(it, 2);
-    EXPECT_EQ(*it, 3);
-}
-
-TEST(iterator, copy_constructor)
-{
-    intlist list{ 1, 2, 3, 4, 5 };
-    auto it1 = list.begin();
-    std::advance(it1, 2);
-    auto it2 = it1;
-    EXPECT_EQ(*it1, *it2);
-}
-
-TEST(iterator, equal_operator)
-{
-    intlist list{ 1, 2, 3, 4, 5 };
-    auto it1 = list.begin();
-    std::advance(it1, 2);
-    auto it2 = list.begin();
-    it2 = it1;
-    EXPECT_EQ(*it1, *it2);
-}
-
 TEST(iterator, dereference_should_return_the_correct_value)
 {
     const int expected = 1;
     intlist list{ expected, 2, 3 };
+
     auto it = list.begin();
+
     EXPECT_EQ(*it, expected);
+}
+
+TEST(iterator, begin_should_return_iterator_to_begin)
+{
+    const int begin = 1;
+    intlist list{ begin, 2, 3 };
+
+    EXPECT_EQ(*(list.begin()), begin);
+}
+
+TEST(iterator, end_should_return_iterator_to_end)
+{
+    intlist list{ 1, 2, 3 };
+
+    EXPECT_ANY_THROW(*(list.end()));
+}
+
+TEST(iterator, pre_increment_operator)
+{
+    intlist list{ 1, 2, 3 };
+    auto it = list.begin();
+
+    EXPECT_EQ(*(++it), 2);
+    EXPECT_EQ(*(++it), 3);
+}
+
+TEST(iterator, post_increment_operator)
+{
+    intlist list{ 1, 2, 3 };
+    auto it = list.begin();
+
+    EXPECT_EQ(*(it++), 1);
+    EXPECT_EQ(*(it++), 2);
+    EXPECT_EQ(*(it++), 3);
+}
+
+TEST(iterator, pre_decrement_operator)
+{
+    intlist list{ 1, 2, 3 };
+    auto it = list.end();
+
+    EXPECT_EQ(*(--it), 3);
+    EXPECT_EQ(*(--it), 2);
+}
+
+TEST(iterator, post_decrement_operator)
+{
+    intlist list{ 1, 2, 3 };
+    auto it = list.end();
+
+    it--;
+    EXPECT_EQ(*(it--), 3);
+    EXPECT_EQ(*(it--), 2);
+    EXPECT_EQ(*(it--), 1);
 }
